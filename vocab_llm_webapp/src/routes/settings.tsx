edf2a2b8d5160f755ed9213b94e19cf75d9e312b
Vladimir Krasnoselskikh <vkrasnoselskikh@quantumbrains.com>
2025-11-27 01:20:04 +0400

    Stash

---
+++ vocab_llm_webapp/src/routes/settings.tsx
@@ -0,0 +1,9 @@
+import { A } from "@solidjs/router";
+
+export default function Settings() {
+  return (
+    <main class="text-center mx-auto text-gray-700 p-4">
+        <span>About Page</span>
+    </main>
+  );
+}
